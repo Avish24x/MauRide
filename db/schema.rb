cff9450e87ca59994c5f1aaf7943af86db73678d
milesh taukoorah <mileshtaukoorah@gmail.com>
2023-06-07 18:33:35 -0700

    added geomap with search option time and distance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_002320) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_211622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_002320) do
     t.integer "seats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_002320) do
     t.string "payment_details"
     t.integer "rating"
     t.string "account_status"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
