b9e7354fc7c5cf8c9c4abc556618618dbd69a80d
lea saulnier <leasaulnier@orange.fr>
2023-06-13 18:11:14 +0400

    affichage voiture dans profil user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @review = Review.new
     @reviews = @user&.vehicules&.map(&:rides)&.first&.map(&:reviews)&.first unless @user.vehicules.empty? && @user.vehicules.map(&:rides)[0]
+    @vehicules = Vehicule.all
   end
 
   private
