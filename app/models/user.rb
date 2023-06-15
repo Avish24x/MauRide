9b01090d3bb734375589b6f84ffb135d06e6a80d
Abhay Bhoojedhun <abhay2105b@icloud.com>
2023-06-15 12:07:21 +0400

    message done just need front

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_many :vehicules
+  has_many :messages, dependent: :destroy
+  has_many :chatrooms, through: :messages, dependent: :destroy
   has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
